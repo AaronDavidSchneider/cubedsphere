8f75911e5a0b5aa6b4f5a77d7d73a67291d0174e
Aaron Schneider <Aaron.David.Schneider@gmail.com>
2021-01-12 16:11:34 +0100

    push new version to conda and pypi

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='cubedsphere',
-    version='v0.1.2',
+    version='v0.1.3',
     packages=setuptools.find_packages(),
     include_package_data=True,
     url='https://github.com/AaronDavidSchneider/cubedsphere',
-    download_url='https://github.com/AaronDavidSchneider/chemcomp/archive/0.1.2.zip',
+    download_url='https://github.com/AaronDavidSchneider/chemcomp/archive/0.1.3.zip',
     license='MIT',
     author='Aaron David Schneider',
     author_email='aaron.schneider@nbi.ku.dk',
